73fce94421de2907074d1f5dcdd81cf6071dbdd5
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2016-01-29 20:45:40 +0100

    Player movement

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Human : MonoBehaviour
+{
+    public float Height;
+    public float Speed;
+
+    [HideInInspector]
+    public Vector3 InputControl;
+    [HideInInspector]
+    public bool InputAim;
+
+    [HideInInspector]
+    public Vector3 Forward = Vector3.forward;
+    [HideInInspector]
+    public Vector3 Right = Vector3.right;
+
+    [HideInInspector]
+    public Action IdleLook;
+    [HideInInspector]
+    public bool LockRot;
+
+    private Rigidbody _rigidbody;
+    private float aimTime;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        var move = (Forward*InputControl.z + Right*InputControl.x).normalized;
+        if (InputControl.magnitude == 0)
+        {
+            if (IdleLook != null) IdleLook();
+        }
+        else if (!LockRot)
+        {
+            if(move.magnitude > 0)
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(move), 0.4f);
+        }
+        _rigidbody.velocity = move*Speed;
+
+        InputControl = Vector3.zero;
+        InputAim = false;
+        LockRot = false;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using UnityEngine;
+using Pathfinding;
+using System.Collections;
+
+[RequireComponent(typeof(Human))]
+[RequireComponent(typeof(Seeker))]
+public class Player : MonoBehaviour
+{
+    public float nextWaypointDistance = 0.3f;
+    public Vector3 targetPosition;
+
+    public float SlowSpeed;
+    public float NormalSpeed;
+    public float FastSpeed;
+
+    private Human human;
+    internal Seeker seeker;
+    internal Path path;
+    private int currentWaypoint = 0;
+    private bool _awaitingPath;
+
+    private void Start()
+    {
+        human = GetComponent<Human>();
+        seeker = GetComponent<Seeker>();
+        seeker.pathCallback += OnPathComplete;
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+    public void Update()
+    {
+        if (path != null)
+        {
+            if (currentWaypoint >= path.vectorPath.Count)
+            {
+                path = null;
+                return;
+            }
+
+            Debug.DrawLine(transform.position, transform.position + transform.forward * nextWaypointDistance, Color.blue, 0.1f, true);
+
+            human.InputControl = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+            if (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) < nextWaypointDistance)
+                currentWaypoint++;
+        }
+        else if (!_awaitingPath)
+        {
+            // Idle here
+        }
+
+        var cam = Camera.main;
+        if (cam != null)
+        {
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit, 100))
+            {
+                targetPosition = hit.point;
+                _awaitingPath = true;
+                seeker.StartPath(transform.position, targetPosition);
+                Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.red, 0.1f);
+            }
+        }
+
+        human.Speed = NormalSpeed;
+    }
+
+    public void Interrupt()
+    {
+        path = null;
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        if (p.error) return;
+        path = p;
+        currentWaypoint = 0;
+        _awaitingPath = false;
+    }
+}
