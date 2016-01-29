0403b1a09c43740ebca442642e96a750a0f81baf
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2016-01-29 22:07:05 +0100

    Todo list

---
+++ Assets/Scripts/Todo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Todo : MonoBehaviour
+{
+
+    public Player player;
+    public Text text;
+
+    private void Update()
+    {
+        if (player.Todo.Count > 0)
+        {
+            var list = "";
+            foreach (var item in player.Todo)
+                list += "- " + item + "\n";
+            text.text = (!player.Control?"You forgot to:\n":"Todo:\n") + list;
+        }
+        else
+        {
+            text.text = (player.Control ? "Ready to leave!" : "You did it!");
+        }
+    }
+}
