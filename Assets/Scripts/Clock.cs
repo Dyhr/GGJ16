33e9b1caf0d843378f4f6f8147ebf652f0f9d01a
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2016-01-29 21:22:45 +0100

    Clocks and stuff

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Clock : MonoBehaviour
+{
+    public bool Running = true;
+    public int StartTimeHour = 5;
+    public int StartTimeMinute = 0;
+    public int EndTimeHour = 6;
+    public int EndTimeMinute = 0;
+    public float MinuteTime = 1;
+
+    private int hour;
+    private int minute;
+    private float timer;
+
+    public Text text;
+
+    private void Start()
+    {
+        hour = StartTimeHour;
+        minute = StartTimeMinute;
+        StartCoroutine(Time());
+    }
+
+    IEnumerator Time()
+    {
+        while (Running)
+        {
+            yield return new WaitForSeconds(MinuteTime);
+
+            minute++;
+            while (minute >= 60)
+            {
+                minute -= 60;
+                hour++;
+            }
+
+            text.text = (hour < 10 ? "0" + hour : hour.ToString()) + ":" + (minute < 10 ? "0" + minute : minute.ToString());
+
+            if (hour >= EndTimeHour && minute >= EndTimeMinute)
+            {
+                Running = false;
+
+                Debug.Log("You Lose");
+            }
+        }
+    }
+}
