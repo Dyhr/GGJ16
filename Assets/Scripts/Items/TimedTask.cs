7b2eaae24ae27c811ccf2ef6d66c5217fed4143b
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2016-01-30 12:33:35 +0100

    Teeth being brushed

---
+++ Assets/Scripts/Items/TimedTask.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TimedTask : Interactable
+{
+    public float Time;
+
+    public override void Interact(Player player)
+    {
+        StartCoroutine(Do());
+    }
+
+    private IEnumerator Do()
+    {
+        yield return new WaitForSeconds(Time);
+        Done = true;
+    }
+}
